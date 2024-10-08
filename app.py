c62552bfe1bc6a5a9013a6706680abf05faa240a
Mohamed Ashraf <ashraph0p1@gmail.com>
2024-10-08 15:20:01 +0300

    Join page function implemented. (Will do google and linkedin later.)

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from crypt import methods
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
-from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, EmailField, PasswordField
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from wtforms import StringField, EmailField, PasswordField, BooleanField
 from wtforms.validators import DataRequired
 import os
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String
 
 
 class Base(DeclarativeBase):
@@ -17,45 +15,67 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+csrf = CSRFProtect(app)
+bootstrap = Bootstrap5(app)
 db.init_app(app)
 
 
-class User(db.Model):
+class CreateUser(db.Model):
+    __tablename__ = 'create_user'
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str]
+    name: Mapped[str] = mapped_column(nullable=False)
+    family_name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    confirm: Mapped[bool] = mapped_column(nullable=True)  # Set nullable=True if it can be None
 
 
 class Makeaccount(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    family_name = StringField('family_name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Ex. John"})
+    family_name = StringField('Family Name', validators=[DataRequired()], render_kw={"placeholder": "Ex. Smith"})
+    email = EmailField('Email', validators=[DataRequired()], render_kw={"placeholder": "Ex. johnsmith1998@gmail.com"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Enter your Password"})
+    confirm = BooleanField('I have read and agree to terms of use and Privacy Statement')
 
 
-app.config['SECRET_KEY'] = os.urandom(12)
-bootstrap = Bootstrap5(app)
-csrf = CSRFProtect(app)
+with app.app_context():
+    db.create_all()
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():  # put application's code here
     return render_template('index.html')
 
 
-@app.route('/join', methods=['GET, POST'])
+@app.route('/join', methods=['GET', 'POST'])
 def join():
     form = Makeaccount()
+    if form.validate_on_submit():
+        with app.app_context():
+            new_user = CreateUser(name=form.name.data,
+                                  family_name=form.family_name.data,
+                                  email=form.email.data,
+                                  password=form.password.data,
+                                  confirm=form.confirm.data)
+            db.session.add(new_user)
+            db.session.commit()
+        return redirect(url_for("profile", id=new_user.id))
+
     return render_template('join.html', form=form)
 
-@app.route('/sign-in', methods=['GET'])
-def sign_up():
-    return '<h1>W.I.P</h1>'
 
-@app.route('/profile/idplacementhere', methods=['GET'])
-def profile():
-    return '<h1>W.I.P</h1>'
+@app.route('/sign-in', methods=['GET', 'POST'])
+def sign_up():
+    return render_template('sign_in.html')
+
+
+@app.route('/profile/<int:id>', methods=['GET'])
+def profile(id):
+    user = CreateUser.query.get_or_404(id)
+    return f'<h1>Welcome, {user.name} {user.family_name}</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
